97d8674e1961d7da44add3056464500c1a8a7597
ochbileg <ochbileg888@gmail.com>
2021-04-20 15:29:38 +0800

    Toglogchid 1 buuhar eelj soligddog bolloo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // тоглогчийн ээлжийг хадгалах хувьсагч, 1-р тоглогчийг 0, 2-р тоглогчийг 1 гэж тэмдэглэнэ.
-var activePlayer = 1;
+var activePlayer = 0;
 
 // тоглогчин цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -18,9 +18,32 @@ document.getElementById('current-1').textContent = 0;
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+
+// Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function(){
+    // Санамсаргүй 1-6 тоо гаргаж өгдөг
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     
+    // Шооны зургийг сольдог
     diceDom.style.display = "block";
     diceDom.src = 'dice-' + diceNumber +'.png';
+
+    // Тоогоо нэмээд харуулна
+    if(diceNumber === 1){
+        // Раунд дуусмагц оноог тэглэх
+        roundScore = 0;
+        // Раунд оноог дэлгэц дээр тэглэх
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+        // идэвхтэй тоглогчийг байрын солино
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+        // идэвхтэй тоглогчийн  тодруулж харуулна
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+        
+    } else {
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }
+    
+
 });
